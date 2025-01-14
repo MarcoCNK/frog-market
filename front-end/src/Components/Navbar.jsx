ca71b54519be10628d8a00bd823231088f377abd
Marco de CNK <virtualnautilus@gmail.com>
2025-01-14 15:39:45 -0300

    Starting with frontend style

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,59 +1,163 @@
 import { Link, NavLink } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faClipboard, faPhone, faMagnifyingGlass, faUser } from '@fortawesome/free-solid-svg-icons';
-
+import { FaHome, FaClipboard, FaPhone, FaUser, FaReact } from 'react-icons/fa';
+import { useEffect, useRef, useState } from "react";
+import { IoIosLogOut, IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import { CiShoppingCart } from "react-icons/ci";
+import frogLogo from "/public/frogLogo.png";
 
 export const NavBar = () => {
+    // SCROLL
+
+    const [isVisible, setIsVisible] = useState(true);
+    const [lastScrollPosition, setLastScrollPosition] = useState(0);
+
+    const handleScroll = () => {
+        const currentScrollPosition = window.pageYOffset;
+
+        if (currentScrollPosition > lastScrollPosition) {
+            // Scrolling down
+            setIsVisible(false);
+        } else {
+            // Scrolling up
+            setIsVisible(true);
+        }
+
+        setLastScrollPosition(currentScrollPosition);
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup event listener on component unmount
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [lastScrollPosition]); // Dependency to track scroll position
+
+    
+    // DROPDOWN
+    const [dropdownState, setDropdownState] = useState(false)
+    const dropdownRef = useRef(null)
+
+    const toggleDropdown = () => {
+        setDropdownState((prevState) => !prevState)
+    }
+
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setDropdownState(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
     return (
-        <>
-            <nav >
-                <Link to="/home">
-                    <img src='' alt="logo" />
-                </Link>
-                <button>
-                    <span className="navbar-toggler-icon">
-                        Brand Dropdown
+        <nav className={`bg-gray-800 text-white p-4 fixed top-0 w-full transition-transform duration-300 z-50 ${
+            isVisible ? 'translate-y-0' : '-translate-y-full'
+        }`}>
+            <div className="container mx-auto flex justify-between items-center">
+                <div className="flex items-center space-x-4">
+                    <span>
+                        <NavLink to="/home">
+
+                            <img src={frogLogo} className="w-12 h-12 rounded-full" alt="Froggy market logo" />
+                        </NavLink>
+
+
                     </span>
-                </button>
-                <ul >
-                    <li className="nav-item home rounded mx-auto d-block align-self-center container-flui-lg" >
-                        <NavLink to="/" className='nav-link anchor rounded mx-auto d-block'><FontAwesomeIcon icon={faHome} />Home</NavLink>
-                    </li>
-                    <li className="nav-item category contact align-self-center ">
-                        <NavLink to='/contact' className='nav-link anchor rounded mx-auto d-block'><FontAwesomeIcon icon={faPhone} />Contactarse  </NavLink>
-                    </li>
-                    <form className="d-flex search rounded mx-auto d-block align-self-center" role="search">
-                        <input type="search" placeholder="Search" />
-                        <button className="btn btn-outline-primary --bs-primary-bg-subtle" type="submit"><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
-                    </form>
-                    <label htmlFor="category">
-                        <li className="nav-item anchor dropdown dropp drop-link rounded mx-auto d-block align-self-center   align-content" >
-                            <a className="nav-link dropdown-toggle  anchor  rounded mx-auto d-block" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Nuestros productos
-                            </a>
+                    <NavLink to="/home">
 
-                            <ul id="category" className="dropdown-menu z-3">
-                                <img className='loguito' src='' />
-                                <NavLink to='productos' value='air' className="droping fs-6 anchor2" data-bs-toggle="dropdown" aria-expanded="false">Repuesto para aires acondicionados</NavLink>
-                                <li><hr className="dropdown-divider" /></li>
-                                <img className='loguito' src='' /><NavLink to='productos' value='fridge' className="droping fs-6 anchor2" data-bs-toggle="dropdown" aria-expanded="false">Repuesto para heladeras</NavLink>
-                                <li><hr className="dropdown-divider" /></li>
-                                <li><FontAwesomeIcon icon={faClipboard} /><NavLink to='productos' className=" droping anchor2 fs-6">Más productos</NavLink></li>
-                            </ul>
-                        </li>
-                    </label>
+                        <span>Store</span>
 
-                    <li className="login-link align-self-center login float-end align-content">
-                        <NavLink className=" align-self-center droping anchor2 fs-6" href="#"><FontAwesomeIcon icon={faUser} />Login</NavLink></li>
-                    <NavLink to='/carrito' className='align-self-center carrito float-end rounded mx-auto d-block align-self-center'>
-
+                    </NavLink>
+                </div>
+                <ul className="flex space-x-6 items-center">
+                    {/* Carrito */}
+                    <NavLink to='/carrito' className='nav-link flex items-center space-x-2'>
+                        <CiShoppingCart />
+                        <span>SuperCart</span>
                         {/* <Badge badgeContent={listaCompras.length} color="secondary">
                             <ShoppingCart color="action" />
                         </Badge> */}
                     </NavLink>
+                    <li ref={dropdownRef}>
+                        <div className="container mx-auto flex justify-between items-center">
+                        <span>
+
+                                <FaUser />
+                        </span>
+                        <span>
+                            <button
+                                id="menu-toggle"
+                                type="button"
+                                data-target="#mobile-menu"
+                                aria-controls="mobile-menu"
+                                aria-expanded={dropdownState}
+                                className="bg-blue-500 text-white p-2 rounded"
+                                onClick={toggleDropdown}
+                            >
+
+                                {dropdownState
+                                    ? <IoMdArrowDropdown />
+                                    : <IoMdArrowDropup />
+                                }
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path>
+                            </button>
+                        </span>
+                        </div>
+
+
+                    </li>
+                    <li>
+                       
+                    </li>
+
+
                 </ul>
-            </nav>
-        </>
+            </div>
+
+            {/* Dropdown */}
+            <div className={`absolute bg-black right-0 mt-2 w-48 shadow-lg rounded-lg ${dropdownState ? '' : 'hidden'}`}>
+                <ul className="p-2">
+                    <li>
+                        <NavLink to="/login" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">
+                            <span>
+                                Login
+
+                            </span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/profile" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">
+                            <span>
+
+                                Profile
+                            </span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/logout" className="flex justify-between items-center block px-4 py-2 hover:bg-gray-100 hover:text-black">
+
+
+                            <span>
+
+                                Logout
+                            </span>
+                            <span>
+
+                                <IoIosLogOut />
+                            </span>
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
     )
 }
 
--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import getAuthHeaders from '../utils/authHeeders.js'
 import useProductDelete from '../Hooks/useProductDelete.jsx'
 import NavBar from '../Components/Navbar.jsx'
-
+import fogPicture from '/public/bogCartoon.webp'
 
 const HomePage = () => {
 	const { products, loader, productErrorState } = useProducts()
@@ -14,28 +14,43 @@ const HomePage = () => {
 	return (
 		<>
 			<NavBar></NavBar>
-			<h1 className='flex items-center justify-center' >Bienvenido a Froggy market</h1>
+			<div className="relative w-full h-[500px]">
+				<img
+					src={fogPicture}
+					alt="Foggy market"
+					className="absolute inset-0 w-full h-full object-cover"
+				/>
+				<div className="absolute inset-0 flex flex-col items-center justify-center text-white bg-black bg-opacity-50">
+					<h1 className="text-4xl font-bold">Welcome to Froggy Market</h1>
+					<p className="text-lg mt-2">We have all kinds of bogs, frogs and bugs</p>
+				</div>
+			</div>
+
 			<Link to="/home/create-product">Create a product</Link>
 			{loader
 				? <p >Loading...</p>
-				: <div className='flex items-center px-4 py-3 text-white'>
-					{products.map(product => (
-						console.log("Product: ", product.seller_id),
-						<div key={product._id}>
-							<p className='text-3xl font-bold underline' >{product.title}</p>
-							<span>{product.price}</span>
-							<Link to={`/home/product/${product.seller_id}`}>See detail</Link>
-							{console.log("Seller product ID",product.seller_id)}
-							<button onClick={() => useProductDelete(product.seller_id)} >Click here to delete</button> 
-							{/* <span>{productErrorStateDelete}</span> */}
-						</div>
-					))}
-				</div>
+				: <ProductsList products={products} />
 
 			}
 		</>
 
 	)
 }
+
+const ProductsList = (  { products }) => { 
+	<div className='flex items-center px-4 py-3 text-white'>
+					{products.map(product => (
+						console.log("Product: ", product.seller_id),
+						<div key={product._id}>
+							<p className='text-3xl font-bold underline' >{product.title}</p>
+							<span>{product.price}</span>
+							<Link to={`/home/product/${product.seller_id}`}>See detail</Link>
+							{console.log("Seller product ID", product.seller_id)}
+							<button onClick={() => useProductDelete(product.seller_id)} >Click here to delete</button>
+							{/* <span>{productErrorStateDelete}</span> */}
+						</div>
+					))}
+				</div>
+ }
 
 export default HomePage
